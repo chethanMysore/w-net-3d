still fixing loss backprop

pipeline.py
@@ -231,16 +231,16 @@ class Pipeline:
                         # self.scaler.scale(soft_ncut_loss).backward(retain_graph=True)
                         soft_ncut_loss.backward(retain_graph=True)
                     # soft_ncut_loss.backward()
-                    if self.clip_grads:
-                        # self.scaler.unscale_(self.optimizer)
-                        torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1)
+                    # if self.clip_grads:
+                    #     # self.scaler.unscale_(self.optimizer)
+                    #     torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1)
                     # self.scaler.step(self.optimizer)
                     # self.scaler.update()
                     self.optimizer.step()
                 else:
                     soft_ncut_loss.backward(retain_graph=True)
-                    if self.clip_grads:
-                        torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1)
+                    # if self.clip_grads:
+                    #     torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1)
                     self.optimizer.step()
 
                 # Update the WNet by backpropagating reconstruction_loss