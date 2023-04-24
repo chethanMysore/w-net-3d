fixed loss normalization

pipeline.py
@@ -239,8 +239,11 @@ class Pipeline:
                         continuity_loss = self.continuity_loss(normalised_res_map)
 
                         # Total Loss = SimilarityLoss + m*ContinuityLoss
-                        loss = Pipeline.combine_losses([similarity_loss, continuity_loss],
-                                                       [self.sim_loss_coeff, self.cont_loss_coeff])
+                        # loss = Pipeline.combine_losses([similarity_loss, continuity_loss],
+                        #                                [self.sim_loss_coeff, self.cont_loss_coeff])
+                        loss_sum = similarity_loss + continuity_loss
+                        loss = self.sim_loss_coeff * (similarity_loss / loss_sum) + self.cont_loss_coeff * (
+                                    continuity_loss / loss_sum)
                         reconstruction_loss = 0
                         reg_loss = 0
 
@@ -267,10 +270,15 @@ class Pipeline:
                         #                                    (self.cont_loss_coeff * continuity_loss)) + \
                         #        self.reconstr_loss_coeff * reconstruction_loss + \
                         #        self.reg_alpha * reg_loss
-                        loss = Pipeline.combine_losses([similarity_loss, continuity_loss, reconstruction_loss, reg_loss]
-                                                       , [self.sim_loss_coeff * self.encoding_loss_coeff,
-                                                          self.cont_loss_coeff * self.encoding_loss_coeff,
-                                                          self.reconstr_loss_coeff, 1.0])
+                        # loss = Pipeline.combine_losses([similarity_loss, continuity_loss, reconstruction_loss, reg_loss]
+                        #                                , [self.sim_loss_coeff * self.encoding_loss_coeff,
+                        #                                   self.cont_loss_coeff * self.encoding_loss_coeff,
+                        #                                   self.reconstr_loss_coeff, 1.0])
+                        loss_sum = similarity_loss + continuity_loss + reconstruction_loss
+                        loss = self.sim_loss_coeff * self.encoding_loss_coeff * (
+                                similarity_loss / loss_sum) + self.cont_loss_coeff * self.encoding_loss_coeff * (
+                                       continuity_loss / loss_sum) + self.reconstr_loss_coeff * (
+                                       reconstruction_loss / loss_sum) + 0.1 * reg_loss
 
                 self.scaler.scale(loss).backward()
                 # if self.with_apex:
@@ -448,10 +456,11 @@ class Pipeline:
 
                             # Total Loss = m*(theta*SimilarityLoss + (1-theta)*ContinuityLoss) +
                             # beta*(reconstruction_loss) + alpha*(regularisation_loss)
-                            loss = Pipeline.combine_losses([similarity_loss, continuity_loss, reconstruction_loss],
-                                                           [self.sim_loss_coeff * self.encoding_loss_coeff,
-                                                            self.cont_loss_coeff * self.encoding_loss_coeff,
-                                                            self.reconstr_loss_coeff])
+                            loss_sum = similarity_loss + continuity_loss + reconstruction_loss
+                            loss = self.sim_loss_coeff * self.encoding_loss_coeff * (
+                                    similarity_loss / loss_sum) + self.cont_loss_coeff * self.encoding_loss_coeff * (
+                                           continuity_loss / loss_sum) + self.reconstr_loss_coeff * (
+                                           reconstruction_loss / loss_sum)
                         torch.cuda.empty_cache()
 
                 except Exception as error: